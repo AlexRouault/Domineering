2460f683677c49bd28cf330b41a19b6ef281904e
Alex Rouault <rouaultalex@gmail.com>
2019-04-06 22:43:03 -0600

    Counts nodes visited
    Allows for comparison of speeds of different algorithms

--- domineering_AI.py
+++ domineering_AI.py
@@ -16,36 +16,44 @@ def random_play(board, null_A, null_B):
 def minimax(board, max_depth, eval_fn):
     # First layer of minimax (maximizes)
     # Returns best PLAY
+    count = 0
     max_score = -math.inf
     max_play = []
     for pp in possible_plays(board, board.turn):
+        count += 1
         new_board = board.copy()
         new_board.play(pp)
-        new_score = mini(new_board, board.turn, max_depth-1, eval_fn)
+        new_score, add_count = mini(new_board, board.turn, max_depth-1, eval_fn)
+        count += add_count
         if new_score > max_score:
             max_score = new_score
             max_play = [pp]
         elif new_score == max_score:
             max_play.append(pp)
+    print(count)
     return random.choice(max_play)
 
 def alphabeta(board, max_depth, eval_fn):
     # First layer of alphabeta (maximizes)
     # Returns best PLAY
+    count = 0
     a = -math.inf
     b = math.inf
     max_score = -math.inf
     max_play = []
     for pp in possible_plays(board, board.turn):
+        count += 1
         new_board = board.copy()
         new_board.play(pp)
-        new_score = minab(new_board, board.turn, max_depth-1, a, b, eval_fn)
+        new_score, add_count = minab(new_board, board.turn, max_depth-1, a, b, eval_fn)
+        count += add_count
         if new_score > max_score:
             max_score = new_score
             max_play = [pp]
         elif new_score == max_score:
             max_play.append(pp)
         a = max(a, new_score)
+    print(count)
     return random.choice(max_play)
     
 
@@ -101,61 +109,73 @@ def greedy_score2(board, player):
 def mini(board, player, max_depth, eval_fn):
     # Even layers of minimax (opponent's turn)
     # Returns worst SCORE opponent can force
+    count = 0
     if max_depth == 0: # At max_depth -> return my evaluated score on current board
-        return eval_fn(board, player)
+        return eval_fn(board, player), count
     else:
         min_score = math.inf
         for pp in possible_plays(board, board.turn):
+            count += 1
             new_board = board.copy()
             new_board.play(pp)
-            new_score = maxi(new_board, player, max_depth-1, eval_fn)
+            new_score, add_count = maxi(new_board, player, max_depth-1, eval_fn)
+            count += add_count
             min_score = min(new_score, min_score)
-        return min_score
+        return min_score, count
     
 def maxi(board, player, max_depth, eval_fn):
     # Odd layers of minimax (my turn)
     # Returns best SCORE I can force
+    count = 0
     if max_depth == 0: # At max_depth -> return my evaluated score on current board
-        return eval_fn(board, player)
+        return eval_fn(board, player), count
     else:
         max_score = -math.inf
         for pp in possible_plays(board, board.turn):
+            count += 1
             new_board = board.copy()
             new_board.play(pp)
-            new_score = mini(new_board, player, max_depth-1, eval_fn)
+            new_score, add_count = mini(new_board, player, max_depth-1, eval_fn)
+            count += add_count            
             max_score = max(new_score, max_score)
-        return max_score
+        return max_score, count
 
 def minab(board, player, max_depth, a, b, eval_fn):
     # Even layers of alphabeta (opponent's turn)
     # Returns worst SCORE opponent can force
+    count = 0
     if max_depth == 0: # At max_depth -> return my evaluated score on current board
-        return eval_fn(board, player)
+        return eval_fn(board, player), count
     else:
         min_score = math.inf
         for pp in possible_plays(board, board.turn):
+            count += 1
             new_board = board.copy()
             new_board.play(pp)
-            new_score = maxab(new_board, player, max_depth-1, a, b, eval_fn)
+            new_score, add_count = maxab(new_board, player, max_depth-1, a, b, eval_fn)
+            count += add_count            
             min_score = min(new_score, min_score)
             b = min(b, min_score)
             if a > b:
                 break # a cut-off
-        return min_score
+        return min_score, count
 
 def maxab(board, player, max_depth, a, b, eval_fn):
     # Odd layers of alphabeta (my turn)
     # Returns best SCORE I can force
+    count = 0
     if max_depth == 0: # At max_depth -> return my evaluated score on current board
-        return eval_fn(board, player)
+        return eval_fn(board, player), count
     else:
         max_score = -math.inf
         for pp in possible_plays(board, board.turn):
+            count += 1
             new_board = board.copy()
             new_board.play(pp)
-            new_score = minab(new_board, player, max_depth-1, a, b, eval_fn)
+            new_score, add_count = minab(new_board, player, max_depth-1, a, b, eval_fn)
+            count += add_count            
             max_score = max(new_score, max_score)
             a = max(a, max_score)
             if a > b:
                 break # b cut-off
-        return max_score
+        return max_score, count
